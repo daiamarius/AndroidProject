b6d1988a3d460905bc880f25c3b505cc5eef64c9
daiamarius <daiamarius98@yahoo.com>
2020-04-26 13:58:35 +0300

    maps marker working

--- app/src/main/java/com/example/statusapp/activities/AboutActivity.java
+++ app/src/main/java/com/example/statusapp/activities/AboutActivity.java
@@ -1,16 +1,59 @@
 package com.example.statusapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.statusapp.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends FragmentActivity implements OnMapReadyCallback  {
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean isServicesOK(){
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(AboutActivity.this);
+        if(available == ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(AboutActivity.this,available,9001);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"Cannot make map requests",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sts = new LatLng(44.443311, 26.038227);
+        map.addMarker(new MarkerOptions().position(sts).title("STS"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sts));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(sts, 12.0f));
     }
 }
